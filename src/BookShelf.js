23048710e079f6874b607733cb5db07fc2f626ae
Tim Abrahamsen <timmy_neutron@mac.com>
2018-03-09 18:49:26 -0500

    feat: ListBooks gets books from BooksAPI refactor: ListBooks is split into BookShelf components based on shelf
    App gets books via request to the BooksAPI, and shows them on BookShelf components based on the books' shelf property

---
+++ src/BookShelf.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+class BookShelf extends Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{ this.props.title }</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            { this.props.books.filter(book => book.shelf === this.props.shelf)
+              .map(book => (
+                <li key={ book.id }>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${ book.imageLinks.thumbnail })` }}></div>
+                      <div className="book-shelf-changer">
+                        <select>
+                          <option value="none" disabled>Move to...</option>
+                          <option value="currentlyReading">Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">{ book.title }</div>
+                    <ol className="book-authors">
+                      { book.authors.map(author => (
+                          <li key={ author }>
+                            { author }
+                          </li>
+                      ))}
+                    </ol>
+                  </div>
+                </li>
+              ))
+            }
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BookShelf
